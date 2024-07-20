896d057338ca832682ee3f5f0e2072aee753bc5b
Patricio Avila <p.avilaf1998@gmail.com>
2024-07-20 18:35:47 -0400

    Projecto terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,58 @@ const blurHeader = () => {
 window.addEventListener("scroll", blurHeader);
 /*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // Cuando el scroll es mayor a 350px, mostrar el boton scroll-up
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== Enlace activo de la sección de desplazamiento ===============*/
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav__list a");
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 58; // Ajusta según el espacio fijo en la parte superior
+    const sectionId = section.getAttribute("id");
+
+    // Encuentra el enlace correspondiente en el menú de navegación
+    const navLink = document.querySelector(
+      `.nav__list a[href="#${sectionId}"]`
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      // Activa el enlace correspondiente
+      navLink.classList.add("active-link");
+    } else {
+      // Desactiva el enlace si no está en vista
+      navLink.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 3000,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`);
+sr.reveal(`.home__card`, { delay: 600, distance: "100px", interval: 100 });
+sr.reveal(`.about__data, .join__image`, { origin: "right" });
+sr.reveal(`.about__image, .join__data`, { origin: "left" });
+sr.reveal(`.popular__card`, { interval: 200 });
